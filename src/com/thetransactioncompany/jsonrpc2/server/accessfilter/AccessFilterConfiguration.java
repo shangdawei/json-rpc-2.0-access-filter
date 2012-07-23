3baa808f82e18904ceb0d30de08b06cb6264097a
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-07-23 11:57:21 +0300

    Adds initial source stubs

---
+++ src/com/thetransactioncompany/jsonrpc2/server/accessfilter/AccessFilterConfiguration.java
@@ -0,0 +1,14 @@
+package com.thetransactioncompany.jsonrpc2.server.accessfilter;
+
+
+/**
+ * Immutable access filter configuration.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-07-23)
+ */
+public class AccessFilterConfiguration {
+
+
+
+}
---
+++ src/com/thetransactioncompany/jsonrpc2/server/accessfilter/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * Filters JSON-RPC 2.0 requests on the server-side according to client IP
+ * whitelist, client X.509 certificate or API token access policy.
+ *
+ * <p>Package dependencies:
+ *
+ * <ul>
+ *     <li>JSON-RPC 2.0 Server, version 1.6 or compatible.
+ *     <li>JSON-RPC 2.0 Base, version 1.27 or compatible.
+ *     <li>Property Util, version 1.6 or compatible.
+ *     <li>JSON Smart, version 1.1.1 or compatible.
+ * </ul>
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ ($version-date$)
+ */
+package com.thetransactioncompany.jsonrpc2.server.accessfilter;
