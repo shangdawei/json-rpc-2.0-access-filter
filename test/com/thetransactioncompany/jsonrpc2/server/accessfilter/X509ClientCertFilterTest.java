6f187e82e9cbb4bd4fe8b0f9fe2fc0f6d8c2c284
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-07-24 12:16:47 +0300

    Adds X509ClientCertFilter JUnit test

---
+++ test/com/thetransactioncompany/jsonrpc2/server/accessfilter/X509ClientCertFilterTest.java
@@ -0,0 +1,104 @@
+package com.thetransactioncompany.jsonrpc2.server.accessfilter;
+
+
+import java.security.Principal;
+
+import com.unboundid.ldap.sdk.DN;
+import com.unboundid.ldap.sdk.LDAPException;
+
+import junit.framework.TestCase;
+
+import com.thetransactioncompany.jsonrpc2.JSONRPC2Request;
+
+import com.thetransactioncompany.jsonrpc2.server.MessageContext;
+
+
+/**
+ * Tests the X.509 client cert filter.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-07-24)
+ */
+public class X509ClientCertFilterTest extends TestCase {
+	
+	
+	public void testRequireCertAndPrincipal()
+		throws LDAPException {
+	
+		X509ClientCertFilter filter = new X509ClientCertFilter();
+		
+		boolean requireCert = true;
+		DN expectedPrincipal = new DN("cn=admin,ou=people,dc=wondlerland,dc=net");
+		
+		filter.init(requireCert, expectedPrincipal);
+		
+		JSONRPC2Request req = new JSONRPC2Request("users.list", 0);
+		
+		// Cert present, correct principal
+		MessageContext ctx = new MessageContext(null, "192.168.0.1", true, "cn=admin,ou=people,dc=wondlerland,dc=net");
+		AccessFilterResult result = filter.filter(req, ctx);
+		assertTrue(result.accessAllowed());
+		
+		// No cert present
+		ctx = new MessageContext(null, "192.168.0.1", true, (String)null);
+		result = filter.filter(req, ctx);
+		assertTrue(result.accessDenied());
+		assertEquals(AccessDeniedError.CLIENT_CERT_REQUIRED.toJSONRPC2Error().getCode(),
+		             result.getJSONRPC2Error().getCode());
+		
+		// Cert present, incorrect principal
+		ctx = new MessageContext(null, "192.168.0.1", true, "cn=bob,ou=people,dc=wondlerland,dc=net");
+		result = filter.filter(req, ctx);
+		assertTrue(result.accessDenied());
+		assertEquals(AccessDeniedError.CLIENT_PRINCIPAL_DENIED.toJSONRPC2Error().getCode(),
+		             result.getJSONRPC2Error().getCode());
+	}
+	
+	
+	public void testRequireCertOnly() {
+	
+		X509ClientCertFilter filter = new X509ClientCertFilter();
+		
+		boolean requireCert = true;
+		DN expectedPrincipal = null;
+		
+		filter.init(requireCert, expectedPrincipal);
+		
+		JSONRPC2Request req = new JSONRPC2Request("users.list", 0);
+		
+		// Cert present
+		MessageContext ctx = new MessageContext(null, "192.168.0.1", true, "cn=admin,ou=people,dc=wondlerland,dc=net");
+		AccessFilterResult result = filter.filter(req, ctx);
+		assertTrue(result.accessAllowed());
+		
+		// No cert present
+		ctx = new MessageContext(null, "192.168.0.1", true, (String)null);
+		result = filter.filter(req, ctx);
+		assertTrue(result.accessDenied());
+		assertEquals(AccessDeniedError.CLIENT_CERT_REQUIRED.toJSONRPC2Error().getCode(),
+		             result.getJSONRPC2Error().getCode());
+	}
+	
+	
+	public void testRequireNoCert() {
+	
+		X509ClientCertFilter filter = new X509ClientCertFilter();
+		
+		boolean requireCert = false;
+		DN expectedPrincipal = null;
+		
+		filter.init(requireCert, expectedPrincipal);
+		
+		JSONRPC2Request req = new JSONRPC2Request("users.list", 0);
+		
+		// Cert present
+		MessageContext ctx = new MessageContext(null, "192.168.0.1", true, "cn=admin,ou=people,dc=wondlerland,dc=net");
+		AccessFilterResult result = filter.filter(req, ctx);
+		assertTrue(result.accessAllowed());
+		
+		// No cert present
+		ctx = new MessageContext(null, "192.168.0.1", true, (String)null);
+		result = filter.filter(req, ctx);
+		assertTrue(result.accessAllowed());
+	}
+}
