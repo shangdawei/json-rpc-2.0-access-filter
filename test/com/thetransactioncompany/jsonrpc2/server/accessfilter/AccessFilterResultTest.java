52b2010baf53fc2a5e2e7de7c3b7b3a0d7476a3f
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-07-24 11:08:01 +0300

    Adds AccessFilterResult JUnit test

---
+++ test/com/thetransactioncompany/jsonrpc2/server/accessfilter/AccessFilterResultTest.java
@@ -0,0 +1,39 @@
+package com.thetransactioncompany.jsonrpc2.server.accessfilter;
+
+
+import junit.framework.TestCase;
+
+
+/**
+ * Tests the access filter result class.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-07-24)
+ */
+public class AccessFilterResultTest extends TestCase {
+
+
+	public void testAccessAllowed() {
+	
+		AccessFilterResult result = new AccessFilterResult();
+		
+		assertTrue(result.accessAllowed());
+		assertFalse(result.accessDenied());
+	}
+	
+	
+	public void testAccessDenied() {
+	
+		AccessFilterResult result = 
+			new AccessFilterResult(AccessDeniedError.HTTPS_REQUIRED.toJSONRPC2Error());
+		
+	
+		assertTrue(result.accessDenied());
+		assertFalse(result.accessAllowed());
+		
+		assertNotNull(result.getJSONRPC2Error());
+		
+		assertEquals(AccessDeniedError.HTTPS_REQUIRED.toJSONRPC2Error().getCode(),
+		             result.getJSONRPC2Error().getCode());
+	}
+}
